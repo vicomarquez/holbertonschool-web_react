f871e1ef3f66ff1b57076ee8e9c4c0477887b271
vicomarquez <vicomarquezmuniz@gmail.com>
2023-05-03 19:26:19 -0300

    fix task 1

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -14,7 +14,7 @@ function updateCounter() {
     $("#count").html(`{$count} clicks on the button`);
 };
 
-$('button').on('click', _.debounce(updateCounter, 300, {
+$('button').on('click', _.debounce(updateCounter, 500, {
     'leading': true,
     'trailing': false
   }));
