1da667c1969eaa81a0568ce1bd75a41eb9944638
vicomarquez <vicomarquezmuniz@gmail.com>
2023-05-03 19:13:12 -0300

    task 1 fix

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,12 +1,13 @@
 import $ from "jquery";
 
+let count = 0;
+
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started<button>');
 $('body').append('<p id='count'></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
 
 function updateCounter() {
     count++;
---
+++ Webpack/task_1/webpack.config.js
@@ -0,0 +1,12 @@
+// webpack.production.config.js
+
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+};
