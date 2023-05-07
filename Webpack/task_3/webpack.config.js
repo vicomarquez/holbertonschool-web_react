4d635d77a07ce1c754ec5f3e4ebbea847aa8d1df
vicomarquez <vicomarquezmuniz@gmail.com>
2023-05-07 19:59:46 -0300

    fix task 3

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports = {
@@ -20,7 +21,11 @@ module.exports = {
        chunks: 'all',
      },
    },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+  devtool: inline-source-map,
   devServer: {
     contentBase: path.join(__dirname, './public'),
     compress: true,
